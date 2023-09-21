9233023f66197921a924847284ba4d70686aca26
NongNon <attnon@gmail.com>
2023-09-21 17:19:59 +0700

    fix some bug a

--- W1-HelloContainer/hello.py
+++ W1-HelloContainer/hello.py
@@ -1,18 +1,26 @@
+# get running os
+import platform
+this_os = platform.system()
 
 # get username and computername from environment
 import os
-username = os.environ.get('USERNAME', 'Unknown')
-world = os.environ.get('COMPUTERNAME', 'World')
-
+if this_os == "Windows":
+    username = os.environ.get("USERNAME", "unknown")
+    computername = os.environ.get("COMPUTERNAME", "World")
+else:
+    username = os.environ.get("USER", "unknown")
+    computername = os.environ.get("HOSTNAME", "World")
+    
 # get python version 
 import sys
 python_version = sys.version
 
 # print greeting
-print("Hello %s from %s!" % (username, world))
+print("Hello %s from %s!" % (username, computername))
+print("OS: %s" % this_os)
 print("Python version: %s" % python_version)
 
 ## keep container running
-# import time
-# while True:
-#     time.sleep(60)
+import time
+while True:
+    time.sleep(60)
